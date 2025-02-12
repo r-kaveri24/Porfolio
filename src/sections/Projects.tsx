694d8bd2b2b106dee7342dae2f4075516b25c115
r-kaveri24 <rautkaveri24@gmail.com>
2025-02-12 21:58:37 +0530

    hero section almost complete

---
+++ src/sections/Projects.tsx
@@ -0,0 +1,89 @@
+import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
+import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
+import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
+import CheckIcon from "@/assets/icons/check-circle.svg"
+import ArrowUpRightIcon from '@/assets/icons/arrow-up-right.svg'
+import GrainImage from '@/assets/images/grain.jpg'
+import Image from "next/image";
+
+const portfolioProjects = [
+  {
+    company: "Acme Corp",
+    year: "2022",
+    title: "Dark Saas Landing Page",
+    results: [
+      { title: "Enhanced user experience by 40%" },
+      { title: "Improved site speed by 50%" },
+      { title: "Increased mobile traffic by 35%" },
+    ],
+    link: "https://youtu.be/4k7IdSLxh6w",
+    image: darkSaasLandingPage,
+  },
+  {
+    company: "Innovative Co",
+    year: "2021",
+    title: "Light Saas Landing Page",
+    results: [
+      { title: "Boosted sales by 20%" },
+      { title: "Expanded customer reach by 35%" },
+      { title: "Increased brand awareness by 15%" },
+    ],
+    link: "https://youtu.be/7hi5zwO75yc",
+    image: lightSaasLandingPage,
+  },
+  {
+    company: "Quantum Dynamics",
+    year: "2023",
+    title: "AI Startup Landing Page",
+    results: [
+      { title: "Enhanced user experience by 40%" },
+      { title: "Improved site speed by 50%" },
+      { title: "Increased mobile traffic by 35%" },
+    ],
+    link: "https://youtu.be/Z7I5uSRHMHg",
+    image: aiStartupLandingPage,
+  },
+];
+
+export const ProjectsSection = () => {
+  return <section className="pb-16 lg:py-24">
+    <div className="container ">
+      <div className="flex justify-center">
+        <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text">Real-world Results</p>
+      </div>
+      <h2 className="font-serif text-3xl md:text-5xl text-center mt-6 ">Featured Projects</h2>
+      <p className="text-center md:text-lg lg:text-xl text-white/60 max-w-md mx-auto mt-4">See how I transformed concepts into engaging digital experiences.</p>
+      <div className="flex flex-col mt-10 md:mt-20 gap-20">
+        {portfolioProjects.map(projects => (
+          <div key={projects.title} className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden  after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:rounded-3xl after:outline-white after:pointer-events-none p-8 pb-0 md:pt-12 md:px-10">
+            <div className="absolute inset-0 -z-10 opacity-5" style={{
+              backgroundImage: `url(${GrainImage.src})`
+            }}></div>
+            <div className="bg-gradient-to-r gap-2 from-emerald-300 to-sky-400 inline-flex font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
+              <span>{projects.company}</span>
+              <span>&bill;</span>
+              <span>{projects.year}</span>
+
+            </div>
+            <h3 className="font-serif text-2xl mt-2 md:mt-4  md:text-4xl">{projects.title}</h3>
+            <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
+            <ul className="flex flex-col gap-4 mt-4 md:mt-5">
+              {projects.results.map((result) => (
+                <li className="flex gap-2 text-sm md:text-base text-white/50">
+                  <CheckIcon className='size-5 md:size-6' />
+                  <span>{result.title}</span></li>
+              ))}
+            </ul>
+            <a href={projects.link}>
+              <button className="bg-white text-gray-950 h-12 w-full rounded-xl font-semibold inline-flex justify-center items-center gap-2 mt-8 md:w-auto md:px-6">
+                <span>Visit Live Site</span>
+                <ArrowUpRightIcon className='size-4' />
+              </button>
+            </a>
+            <Image src={projects.image} alt={projects.title} className="mt-8 -mb-4 md:-mb-0" />
+          </div>
+        ))}
+      </div>
+    </div>
+  </section>;
+};
