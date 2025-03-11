588b15055af6a6e542b62c0d7da90b04fcfe7305
rautkaveri24 <rautkaveri24@gmail.com>
2025-03-11 18:57:08 +0530

    chages to the buttons on home

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -84,12 +84,12 @@ export const HeroSection = () => {
         <p className='mt-4 text-center md:text-lg text-white/60'>I specialize in transforming designs into, functional, high-performance web applications. Hoping to do same in industry..</p>
       </div>
       <div className='relative flex flex-col md:flex-row justify-center items-center mt-8 gap-4'>
-        <button onClick={handleDownload} className='absolute -translate-x-28  inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl z-8'>
+        <button onClick={handleDownload} className='  inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl '>
           <span className='font-semibold'>Explore My Resume</span>
           <ArrowDown className='size-4' />
         </button>
         <a href='#contact'
-          className='w-full inline-flex items-center justify-center min-w-fit gap-2 border sm:w-fit border-white bg-white text-gray-900 h-12 px-6 rounded-xl cursor-pointer'>
+          className=' w-full inline-flex items-center justify-center min-w-fit gap-2 border sm:w-fit border-white bg-white text-gray-900 h-12 px-6 rounded-xl cursor-pointer '>
           <span>👋</span>
           <span className='font-semibold'>Let&apos;s Connect</span>
         </a>
